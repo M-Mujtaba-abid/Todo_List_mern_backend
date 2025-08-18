9db8b5a7e9a86cc2a8efffd26478aa858cc42e36
Mujtaba <mujtabaabid06@gmail.com>
2025-08-19 00:04:40 +0500

    changes in pkg file and vercel file

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ import app from "./src/app.js"
 
 dotenv.config({
     path:"./.env"
-})
+})  
 
 const PORT = process.env.PORT || 5000;
 
 mongoDbConnection()
 
 app.listen(PORT,()=>{
-    console.log("server running on the port",process.env.PORT)
+    console.log("server running on the port",PORT)
 })
